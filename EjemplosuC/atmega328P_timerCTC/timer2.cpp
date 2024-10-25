9b892f085523bdedc5add57eb241c65bafbd0cdd
gastonDeb12 <gvaldez@gvaldez.com>
2024-10-25 00:22:28 -0300

    add pdf and examples using uc2

---
+++ EjemplosuC/atmega328P_timerCTC/timer2.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h> 
+#include "timer2.h"
+
+
+void initport(led_state_t led){ 
+  pinMode(led.number_led, OUTPUT) ; 
+  setStateLed(led) ; 
+}
+
+void setStateLed(led_state_t led){
+  digitalWrite(led.number_led, led.state) ;   
+}
